f47fa6a6c931b7ab76f97f6c78bc7296bb4919de
unknown <ayoub1benmansour@gmail.com>
2026-03-05 12:46:52 +0000

    Added api url in environment and admin and admin dashboard components

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,21 @@
+import { AdminComponent } from './features/components/admin/admin.component';
 import { Routes } from '@angular/router';
+import { AdminDashboardComponent } from './features/components/admin/admin-dashboard/admin-dashboard.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: 'admin',
+        component: AdminComponent,
+        children: [
+            {
+                path: 'dashboard',
+                component: AdminDashboardComponent
+            },
+            {
+                path: '',
+                redirectTo: 'dashboard',
+                pathMatch: 'full'
+            }
+        ]
+    }
+];
